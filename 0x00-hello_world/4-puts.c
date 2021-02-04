4b2b7c08107655b67a79e16f42b1e9f8c796ab3a
natyarteagac <natalia.arteagac94@gmail.com>
2021-02-04 00:28:41 +0000

    mainprints

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*Function main prints "\"programming is like build a multilingual puzzle
+*Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
