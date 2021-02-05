f20404881d09025f17182e041435ab836aff7f44
natyarteagac <natalia.arteagac94@gmail.com>
2021-02-05 02:15:01 +0000

    lastdigitfour

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ int main(void)
 	}
 		else if (a < 6)
 	{
-		printf("Last digit of %i and %i is less than 6 and not 0\n", n, a);
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, a);
 	}
 
 	return (0);
