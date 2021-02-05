647c82937974cc4f4e3f8303bde7e08999462e3a
natyarteagac <natalia.arteagac94@gmail.com>
2021-02-05 17:54:00 +0000

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,17 +2,17 @@
 
 /**
  * main - Prints the size of various types based on
- *        the computer it is compiled and run on..
- *
+ *        the exercise with long, int, short,
+ *        long long int and unsigned long.
  * Return: Always 0.
  */
 int main(void)
 {
-	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-	printf("Size of an int: %zu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	printf("Size of a long: %d byte(s)\n", sizeof(long));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a short: %d byte(s)\n", sizeof(short));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of an unsigned long: %d byte(s)\n", sizeof(unsigned long));
 
 	return (0);
 }
