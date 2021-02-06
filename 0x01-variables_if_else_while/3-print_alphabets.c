b1e5ab571b95390cf48100e7f7a36706aba2400f
natyarteagac <natalia.arteagac94@gmail.com>
2021-02-06 21:59:39 +0000

    lowandupper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+* main - entry point.
+*
+* Return: Always 0.
+*
+*/
+
+int main(void)
+{
+
+	int z;
+
+	int c;
+
+	for (z = 97; z <= 122; z++)
+
+	{
+		putchar(z);
+	}
+
+	for (c = 65; c <= 90; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
