6ce563965ccc9f635f44136b2734020c58658beb
natyarteagac <natalia.arteagac94@gmail.com>
2021-02-06 21:10:18 +0000

    printalphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+# include <stdio.h>
+
+/**
+* main - for print a..z lower case
+* putchar (z)
+* return: always 0.
+*/
+
+int main(void)
+{
+
+	int z;
+
+	int c;
+
+	c = 122;
+
+	for (z = 97; z <= c; z++)
+
+	{
+		putchar(z);
+	}
+
+	return (0);
+
+}
