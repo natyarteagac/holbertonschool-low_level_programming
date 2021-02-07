e6cf74eb4d3eda61105504a43a776a1120aa4380
natyarteagac <natalia.arteagac94@gmail.com>
2021-02-07 03:34:11 +0000

    printtenahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - entry point.
+*
+* Return: Always 0.
+*
+*/
+
+int main(void)
+{
+
+	int z;
+
+	for (z = 122; z >= 97; z--)
+	{
+		putchar(z);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
