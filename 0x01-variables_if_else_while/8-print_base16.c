9024fecdc00f4bb1292f6dfed5ceb2a5a15bb3a8
natyarteagac <natalia.arteagac94@gmail.com>
2021-02-07 03:48:23 +0000

    printbase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+* main - entry point.
+*
+* Return: Always 0.
+*
+*/
+
+int main(void)
+{
+
+	int z;
+
+	int c;
+
+	for (z = 48; z <= 57; z++)
+	{
+		putchar(z);
+	}
+
+	for (c = 97; c <= 102; c++)
+	{
+		putchar(c);
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
