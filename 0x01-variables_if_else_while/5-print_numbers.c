112a894800868a3cc984c3667828df7edc2fadd2
natyarteagac <natalia.arteagac94@gmail.com>
2021-02-07 02:53:32 +0000

    numberonetwonine

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - entry point.
+*
+* Return: Always 0.
+*
+*/
+
+int main(void)
+{
+
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+
+	return (0);
+}
