9773a56212228b938db10218c6890b688cd2aea3
natyarteagac <natalia.arteagac94@gmail.com>
2021-02-09 22:46:43 +0000

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/*
+* print_sign - starting point.
+*
+* n prints the number required.
+*
+* Return: -1, 0, 1.
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
