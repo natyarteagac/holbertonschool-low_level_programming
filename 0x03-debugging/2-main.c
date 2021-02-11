d35f54ebd4d728acf6b70eab47c1596b8bee0bff
natyarteagac <natalia.arteagac94@gmail.com>
2021-02-11 04:05:48 +0000

    twolargestnumber

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -11,13 +11,11 @@ int main(void)
 	int a, b, c;
 	int largest;
 
-	a = 0;
+	a = 972;
 	b = -98;
-	c = 972;
+	c = 0;
 	
-	largest = (a);
-	largest = (b);
-	largest = (c);
+	largest = largest_number(a, b, c);
 
 	printf("%d is the largest number\n", largest);
 
