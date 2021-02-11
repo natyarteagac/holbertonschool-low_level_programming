cebb2f557bcad1408e397f20c642c71aa70b9896
natyarteagac <natalia.arteagac94@gmail.com>
2021-02-11 23:09:48 +0000

    twomul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,9 +1,16 @@
 #include "holberton.h"
 
+/**
+* mul - entry point.
+* @a: integer one.
+* @b: integer two.
+* Return: c.
+*/
+
 int mul(int a, int b)
 {
 	int c;
-	
+
 		c = a * b;
 		return (c);
 }
