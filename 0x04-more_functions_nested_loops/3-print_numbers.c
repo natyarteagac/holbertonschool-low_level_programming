b28bdb22cbaa12473124a959359d2419c9b43929
natyarteagac <natalia.arteagac94@gmail.com>
2021-02-12 00:06:35 +0000

    printnumbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+void print_numbers(void)
+{
+	int a = 0 , b;
+
+	while (a <= 0)
+	{
+		while (b <= 9)
+		{
+			_putchar(b + '0');
+			b++;
+		}
+	putchar('\n');
+	a++;
+	}	
+}
+
