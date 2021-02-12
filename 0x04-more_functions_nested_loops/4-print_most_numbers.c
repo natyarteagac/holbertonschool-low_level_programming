d2a03c26dfa6306cdcf2e29a3dbaf92e8a5eb251
natyarteagac <natalia.arteagac94@gmail.com>
2021-02-12 01:52:01 +0000

    printmostnumbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+* print_most_numbers - entry point.
+*
+* Return: 0.
+*/
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	if (a != '2' && a != '4')
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
