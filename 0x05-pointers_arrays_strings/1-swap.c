81700e7d79173b9b7b08ee9c7807137b6b73e471
natyarteagac <natalia.arteagac94@gmail.com>
2021-02-16 03:19:52 +0000

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+* swap_int - entry point.
+* @a: pointer the integer we want to set to 42.
+* @b: pointer the integer we want to set to 98.
+*
+* Return: nothing.
+*/
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
