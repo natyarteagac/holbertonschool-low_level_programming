71ac702bac475b5e4a2cd1ac336a758c174f84e5
natyarteagac <natalia.arteagac94@gmail.com>
2021-02-16 03:09:01 +0000

    resettoothernumber

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+* reset_to_98 - entry point.
+* @n: pointer the integer we want to set to 98.
+*
+* Return: nothing.
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+0x05. C - Pointers, arrays and strings
+
+In this file we're going to show more about the pointers how to use in different ways.
