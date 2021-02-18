3f047d6576a762ab779bd6ce78575731472bcdb0
natyarteagac <natalia.arteagac94@gmail.com>
2021-02-18 20:42:15 +0000

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+* *_strcat - entry point.
+* @dest: string.
+* @src: string.
+* Return: dest.
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+	for (j = 0; *(src + j) != '\0'; j++, i++)
+	{
+		*(dest + i) = *(src + j);
+	}
+	*(dest + i) = '\0';
+
+	return (dest);
+}
