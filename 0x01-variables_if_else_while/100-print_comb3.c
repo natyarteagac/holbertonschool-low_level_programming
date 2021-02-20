3b7b6aa4b84248976f14db10b5af9e941852fade
natyarteagac <natalia.arteagac94@gmail.com>
2021-02-20 16:42:27 +0000

    printcomb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - entry point.
+* return: 0.
+*/
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = a + 1; b <= 9; b++)
+		{
+			putchar(a + '0');
+			putchar(b + '0');
+			if ( a == 8 && b == 9)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
