02ccc07ea41715cd0809ab96cf193ef370f2a21e
natyarteagac <natalia.arteagac94@gmail.com>
2021-02-21 23:36:53 +0000

    ninestrcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+* *_strcpy - entry point.
+* @dest: empty string.
+* @src: source string.
+* Return: dest.
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int leng = 0, a;
+
+	while (*(src + leng))
+	{
+		leng++;
+	}
+
+	for (a = 0; a <= leng; a++)
+	{
+		*(dest + a) = *(src + a);
+	}
+	*(dest + a) = '\0';
+
+	return (dest);
+}
