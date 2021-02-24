5bda8d134ae03b3bc69fdc7debd0bffb605119fb
natyarteagac <natalia.arteagac94@gmail.com>
2021-02-24 23:54:23 +0000

    stringstoupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+* *strin_toupper - entry point.
+* @p: char pointer.
+* Return: p.
+*/
+
+char *string_toupper(char *p)
+{
+	int i;
+
+	for (i = 0; *(p + i) != '\0'; i++)
+	{
+		if (*(p + i) >= 'a' && *(p + i) <= 'z')
+		*(p + i) = *(p + i) - 32;
+	}
+	return (p);
+}
