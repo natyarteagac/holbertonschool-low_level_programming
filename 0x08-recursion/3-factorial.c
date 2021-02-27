f1414a34a510c54556b20b734c13aabd3bf92c5b
natyarteagac <natalia.arteagac94@gmail.com>
2021-02-27 01:16:49 +0000

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* factorial - entry point.
+* @n: int pointer.
+* Return: 0.
+*/
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	return (n * factorial(n - 1));
+}
+
