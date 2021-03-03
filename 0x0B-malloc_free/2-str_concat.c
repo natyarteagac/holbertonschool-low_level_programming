e912392284e93c3ee36978bec1d17d600d65ac28
natyarteagac <natalia.arteagac94@gmail.com>
2021-03-03 16:51:58 +0000

    strconcat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+* *str_concat - entry point.
+* @s1: char pointer.
+* @s2: char pointer.
+* Return: Result of the concanetenation.
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, k, j, m, p = 0;
+	char *ns;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	for (k = 0; s2[k] != '\0'; k++)
+
+	ns = malloc((i + k) * sizeof(char));
+
+	for (j = 0; j < i; j++)
+		ns[j] = s1[j];
+
+	for (m = i; m < k + i; m++)
+	{
+		(ns[m] = s2[p]);
+		p++;
+	}
+	ns[m] = '\0';
+	return (ns);
+}
