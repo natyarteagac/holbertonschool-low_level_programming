97c848605da96a9f87c9266a25b95d54a7075b27
natyarteagac <natalia.arteagac94@gmail.com>
2021-03-04 20:50:32 +0000

    stringconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+* *string_nconcat - entry point.
+* @s1: char pointer.
+* @s2: char pointer.
+* @n: unsigned int.
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, z, p, r, b = 0;
+	char *p_array;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] != '\0'; i++)
+	;
+	for (z = 0; s2[z] != '\0'; z++)
+	;
+	if (n >= z)
+		n = z;
+
+	p_array = malloc((i + z) * sizeof(char));
+	if (p_array == NULL)
+		return (NULL);
+
+	for (p = 0; p < i; p++)
+		p_array[p] = s1[p];
+
+	for (r = i; r < i + z; r++)
+	{
+		p_array[r] = s2[b];
+		b++;
+	}
+	p_array[r] = '\0';
+
+	return (p_array);
+}
