9e219ffc3125ddfc97729dc4901597b47d304d57
natyarteagac <natalia.arteagac94@gmail.com>
2021-03-04 02:56:21 +0000

    freegrid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+* free_grid - entry point
+* @grid: int double pointer.
+* @height: int pointer.
+* Return: void.
+*/
+
+void free_grid(int **grid, int height)
+{
+	int i;
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}                                                
