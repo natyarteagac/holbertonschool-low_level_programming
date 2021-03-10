0f35e3bfd3cd5b93a7c2d92838452e6e6cc0a8b9
natyarteagac <natalia.arteagac94@gmail.com>
2021-03-10 01:22:38 +0000

    printdog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,8 +14,9 @@ struct dog
 	float age;
 	char *owner;
 };
-#endif
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 int _putchar(char c);
 void print_dog(struct dog *d);
+
+#endif
