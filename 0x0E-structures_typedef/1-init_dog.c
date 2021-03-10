fc03a8f063e7458866bba47b3a58f5e405f1e0f3
natyarteagac <natalia.arteagac94@gmail.com>
2021-03-10 01:30:00 +0000

    initdogcorrection

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "dog.h"
+#include <stdlib.h>
 
 /**
 * struct dog - Function.
@@ -13,8 +14,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
-		d->name = *name;
+		d->name = name;
 		d->age = age;
-		d->owner = *owner;
+		d->owner = owner;
 	}
 }
