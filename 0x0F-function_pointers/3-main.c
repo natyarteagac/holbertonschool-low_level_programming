97f8b47daf8177d237b954a2dfe1ef1832cc7bc5
natyarteagac <natalia.arteagac94@gmail.com>
2021-03-14 21:25:21 +0000

    calculatocorrection

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,13 +12,13 @@ int main(int argc, char *argv[])
 	int a, b, result;
 	int (*f)(int, int);
 
-		a = atoi(argv[1]);
-		b = atoi(argv[3]);
 	if (argc > 4 || argc < 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+		a = atoi(argv[1]);
+		b = atoi(argv[3]);
 	f = get_op_func(argv[2]);
 	if (f == NULL)
 	{
