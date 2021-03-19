77e31a6ef9f18893227ce33c83e03b8ea915781b
natyarteagac <natalia.arteagac94@gmail.com>
2021-03-19 20:31:07 +0000

    returnofnumberofelements

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 
 /**
-*
-*
-*
+* list_len - entry point.
+* @h: pointer to node.
+* Return: Number of elements.
 */
 
 size_t list_len(const list_t *h)
 {
-	int i = 0;
+	size_t i = 0;
 
 	while (h != NULL)
 	{
-	printf("%s", h->str);
-	i++;
+		h = h->next;
+		i++;
 	}
 
 	return (i);
