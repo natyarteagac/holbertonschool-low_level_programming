8cfbf3044a7a2a1674b9b4f52d6a418017a5fb35
natyarteagac <natalia.arteagac94@gmail.com>
2021-03-19 23:51:30 +0000

    add_note

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+* add_node - entry point.
+* @head: doble pointer to head.
+* @str: Pointer to node.
+* Return: new node.
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	unsigned int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+	return (NULL);
+
+	new_node->str = strdup(str);
+
+	if (new_node->str == NULL)
+	{
+		free(new_node->str);
+		return (NULL);
+	}
+
+	while (new_node->str[i])
+	{
+		i++;
+	}
+	new_node->len = i;
+	new_node->next = *head;
+
+	*head = new_node;
+
+	return (new_node);
+}
