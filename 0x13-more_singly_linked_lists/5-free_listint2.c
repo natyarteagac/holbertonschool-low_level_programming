aa31507d38932052ea36c388732c36236a97d2ae
natyarteagac <natalia.arteagac94@gmail.com>
2021-03-23 20:08:18 +0000

    freelistint2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* free_listint2 - entry point.
+* @head: head of nodes.
+* Return: void.
+*/
+
+
+void free_listint2(listint_t **head)
+{
+	listint_t *i = *head;
+
+	while (*(head) != NULL)
+	{
+		i = (*head)->next;
+		free(*head);
+		*head = i;
+	}
+}
