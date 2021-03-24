3f9e3d75f02642fce97df9d4e1354ef9500185a0
natyarteagac <natalia.arteagac94@gmail.com>
2021-03-23 20:12:17 -0500

    freelistint

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,5 +9,15 @@
 
 void free_listint2(listint_t **head)
 {
-	
+	listint_t *i;
+
+	if (head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		i = (*head)->next;
+		free(*head);
+		*head = i;
+	}
 }
