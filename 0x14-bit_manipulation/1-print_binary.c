5d5bdaaf50eb9d54117686dcda6f82ba57254699
natyarteagac <natalia.arteagac94@gmail.com>
2021-03-26 02:21:02 +0000

    printbinary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+* print_binary - entry point.
+* @n: numbers to convert.
+* Return: void.
+*/
+
+void print_binary(unsigned long int n)
+{
+	int i, k;
+
+	if (n == 0)
+	{
+		_putchar ('0');
+		return;
+	}
+
+	for (i = 31; i >= 0; i--)
+	{
+		k = n >> i;
+
+		if (k & 1)
+		{
+
+			_putchar('1');
+		}
+		else if (k != 0)
+		{
+			_putchar('0');
+		}
+	}
+
+}
