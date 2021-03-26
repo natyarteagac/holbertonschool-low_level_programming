55d7a458e8b56e06e846b501db4e8075f209b1d6
natyarteagac <natalia.arteagac94@gmail.com>
2021-03-26 21:43:34 +0000

    bittwoget

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+* get_bit - entry point.
+* @n: numbers to comparate.
+* @index: number to return.
+* Return: index.
+*/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (n == 00)
+		return (-1);
+
+	if ((n & (1 << index)))
+	{
+		return (1);
+	}
+	else
+	{
+	return (0);
+	}
+
+	return (index);
+}
