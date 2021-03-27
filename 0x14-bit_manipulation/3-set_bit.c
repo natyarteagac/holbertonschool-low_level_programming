385504f4932e9ff679d723ef045f5a85f131a713
natyarteagac <natalia.arteagac94@gmail.com>
2021-03-27 00:03:44 +0000

    setbits

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+* set_bit - entry point.
+* @n: pointer to number.
+* @index: index of bits.
+* Return: 1 if works or -1 if dont works.
+*/
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	int x;
+
+	if (index == 32)
+		return (-1);
+
+	x = 1 << index;
+
+	*n = *n | x;
+
+	return (1);
+}
+
