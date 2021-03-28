5180f47bad9dec442e9c7a18a4f3fca648896ead
natyarteagac <natalia.arteagac94@gmail.com>
2021-03-28 02:44:22 +0000

    gethenode

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,7 +16,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 
-	while (i != index)
+	while (i != index && p != NULL)
 	{
 		p = p->next;
 		i++;
