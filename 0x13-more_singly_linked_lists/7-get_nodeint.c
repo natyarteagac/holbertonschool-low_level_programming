29c831365439f41f42f762e379175f2f5801bf50
natyarteagac <natalia.arteagac94@gmail.com>
2021-03-28 01:50:30 +0000

    getthenode

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+* get_nodeint_at_index - entry point.
+* @head: direction of node.
+* @index: index of node to return.
+* Return: node.
+*/
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *p = head;
+
+	unsigned int i;
+
+	if (p == NULL)
+		return (NULL);
+
+	while (index != i)
+	{
+		p = p->next;
+		i++;
+	}
+	return (p);
+}
