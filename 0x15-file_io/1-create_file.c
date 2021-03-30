7686be0a739589a1d596f4b91273aece4c768e05
natyarteagac <natalia.arteagac94@gmail.com>
2021-03-30 19:01:33 +0000

    createthefile

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 		}
 	}
 
-	n = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	n = open(filename, O_CREAT | O_TRUNC | O_WRONLY);
 		if (n == -1)
 			return (-1);
 
