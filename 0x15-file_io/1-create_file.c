e973588a932747ff5bf7181a4a59303dcd01b0c2
natyarteagac <natalia.arteagac94@gmail.com>
2021-03-30 19:18:37 +0000

    createthefilecorrect

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,6 +14,9 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	if (text_content == NULL)
+		text_content = "";
+
 	if (text_content != NULL)
 	{
 		while (text_content[lenght])
