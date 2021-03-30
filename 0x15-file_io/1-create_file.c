e4a654bcd69c2ad20555deab745c5c99a68f0b3d
natyarteagac <natalia.arteagac94@gmail.com>
2021-03-30 02:32:28 +0000

    createafile

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,25 +9,32 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int n, b, c;
-	char buf[1024];
+	int n, c;
+	char *p;
+	int lenght = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	while (text_content == NULL)
-	{
-		n = open(filename, O_CREAT | O_WRONLY, 0600);
+	if (text_content == NULL)
+		text_content = "";
 
-			if (n == -1)
-				return (0);
-		b = read(n, buf, 1024);
-			if (b == -1)
-				return (0);
-		c = write(STDOUT_FILENO, buf, b);
-			if (c == -1)
-				return (0);
+	n = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+		if (n == -1)
+			return (-1);
+	while (lenght != '\0')
+	{
+	p = malloc(sizeof(char) * lenght);
+		if (p == NULL)
+			return (-1);
+
+	c = write(n, p, lenght);
+		if (c == -1)
+			return (-1);
+		free(p);
 		close(n);
+		lenght++;
 	}
+
 	return (1);
 }
