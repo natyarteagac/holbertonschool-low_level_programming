7da424b8bc40a7d52eb82130620b426dc8184a11
natyarteagac <natalia.arteagac94@gmail.com>
2021-05-03 00:02:29 +0000

    Function to add a new node at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,12 +14,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node;
 	dlistint_t *tmp = *h;
 
+	if (*h == NULL)
+	{
+		return(NULL);
+	}
+
 	new_node = malloc(sizeof(dlistint_t));
 		if (new_node == NULL)
 		{
 			return (NULL);
 		}
-	new_node->prev = *h;
+
 	new_node->n = n;
 
 	while (i < idx && tmp != NULL)
@@ -27,9 +32,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		tmp = tmp->next;
 		i++;
 	}
-	new_node->prev = tmp;
-	tmp->next = new_node;
-	tmp = tmp->next;
+	if (i == 0)
+		return (0);
+	else
+	new_node->next = tmp;
+	new_node->prev = tmp->prev;
+	tmp->prev->next = new_node;
+	tmp->prev = new_node;
 
 	return (new_node);
 }
