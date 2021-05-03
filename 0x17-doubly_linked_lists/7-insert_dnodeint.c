5f70e392fd75209dba8938ba61a966e89d5de37d
natyarteagac <natalia.arteagac94@gmail.com>
2021-05-03 00:09:37 +0000

    insert a new node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -33,10 +33,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (i == 0)
 		return (0);
 
-	new_node->next = tmp;
-	new_node->prev = tmp->prev;
-	tmp->prev->next = new_node;
-	tmp->prev = new_node;
+	if (*h != NULL)
+	{
+		new_node->next = tmp;
+		new_node->prev = tmp->prev;
+		tmp->prev->next = new_node;
+		tmp->prev = new_node;
+	}
 
 	return (new_node);
 }
