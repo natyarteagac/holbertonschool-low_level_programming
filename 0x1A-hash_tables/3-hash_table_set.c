b72f94f7f14e7835f38016d5c48dce7c7bc83af7
natyarteagac <natalia.arteagac94@gmail.com>
2021-05-28 16:58:20 -0500

    Checking

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,14 +10,14 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_table_t *new_element = ht;
+	hash_table_t *new_element;
 
-	if (ht == NULL)
-		return (NULL);
+	if (key == NULL)
+		return (-1);
 
 	new_element = malloc(sizeof(hash_table_t));
 	if (new_element == NULL)
 		return (NULL);
 
-	return (new_element);
+	return (1);
 }
