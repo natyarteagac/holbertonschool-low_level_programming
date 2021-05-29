c0c480ab1dd70f44c99adc38270d431f9dfc2b28
natyarteagac <natalia.arteagac94@gmail.com>
2021-05-28 19:06:11 -0500

    Checking

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,13 +11,22 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_table_t *new_element;
+	unsigned int i = 0;
+	hash_node_t *key = key;
+	hash_node_t *valuedup = strdup(value);
 
-	if (key == NULL)
-		return (-1);
+	if (key == NULL || ht == NULL)
+		return (0);
 
 	new_element = malloc(sizeof(hash_table_t));
 	if (new_element == NULL)
 		return (NULL);
 
-	return (1);
+	while (new_element->array[i])
+	{
+		i++;
+	}
+	new_element->array = i;
+
+	return (new_element);
 }
