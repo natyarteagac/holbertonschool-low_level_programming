5e7a4557f11a0d195bd1967e78e871acb325132b
natyarteagac <natalia.arteagac94@gmail.com>
2021-06-03 11:43:31 -0500

    Checking

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,7 +8,25 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
+	hash_node_t *tmp;
+	unsigned int iterator = 0;
+
 	if (ht == NULL)
 		return;
-	printf("%s\n", ht->array);
+
+	if (ht != NULL)
+	{
+		printf("{");
+		while (iterator < ht->size)
+		{
+			tmp = ht->array[iterator];
+			while (tmp != NULL)
+			{
+				printf("%s : %s", tmp->key, tmp->value);
+				tmp = tmp->next;
+				iterator++;
+			}
+		}
+	}
+	printf("}");
 }
