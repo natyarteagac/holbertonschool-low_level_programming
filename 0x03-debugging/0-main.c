b1aad0e2f624311aa1dd698975f43eaab8c8fff1
natyarteagac <natalia.arteagac94@gmail.com>
2021-06-23 13:17:24 -0500

    Testing 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * positive_or_negative - Testing with 0
+ * i: Number to test.
+ * Return: void
+ */
+void positive_or_negative(int i)
+{
+	positive_or_negative(0);
+}
