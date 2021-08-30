6d01fac62f0592d5379a8213111567d36f921c25
natyarteagac <natalia.arteagac94@gmail.com>
2021-08-30 12:36:49 -0500

    Starting with project

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * linear_search - Function to search the value using linear search algorithm
+ * @array: Pointer to array.
+ * @size: Size of array
+ * @value: Value to search for in array
+ * Return: First index where value is located or -1 if function fails.
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t index = 0;
+
+	if (value == 0 || array == NULL)
+		return (-1);
+
+	for (index = 0; index < size; index++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", index, array[index]);
+		if (array[index] == value)
+			return (index);
+	}
+	return (-1);
+}
